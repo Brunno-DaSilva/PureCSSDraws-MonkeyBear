d0aea68e747b12ee1ef22ecfecfc31219bd3c4db
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-04-23 06:14:01 -0500

    Created mouse follow pupills

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(`Cia , mi piace parlare italiano `)
+
+ const pupill = document.getElementsByClassName("followMouse");
+
+ document.onmousemove = () =>{
+   let x = event.clientX * 100 / window.innerWidth + "%";
+   let y = event.clientY * 100 / window.innerHeight + "%";
+   for(let i=0;i<2;i++){
+     pupill[i].style.left = x;
+     pupill[i].style.top = y;
+     pupill[i].style.transform = `translate(-${x},-${y})`;
+   }
+ }
